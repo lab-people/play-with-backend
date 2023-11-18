70e1fdeea1f92831c5b7830988e3d8155eb04128
dongguncho <movegun23@gmail.com>
2023-11-18 17:22:32 +0900

    [Update]

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import test from "./Test/index";
+import user from "./User/index";
 
 export default (app: express.Application) => {
   app.use("/api/test", test);
+  app.use("/api/user", user);
 };
