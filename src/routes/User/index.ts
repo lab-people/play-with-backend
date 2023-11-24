8fafa422cc74c6122b0aa9e6084a25ee9a32dd62
dongguncho <movegun23@gmail.com>
2023-11-24 21:37:48 +0900

    [Update]

--- src/routes/User/index.ts
+++ src/routes/User/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { addUser, getUser, login } from "./User";
+import { addUser, getTest, getUser, login } from "./User";
 import { authenticateJwt } from "../../passport/authenticate";
 
 const router: Router = Router();
 
 router.get("/", authenticateJwt, getUser);
-router.get("/aaa", getUser);
+router.get("/aaa", getTest);
 router.post("/login", login);
 router.post("/", addUser);
 
