0d3ef577aeb3225b72cdda1abcdbcbf704e127ad
chaeyoo <chaeh0124@gmail.com>
2023-11-24 21:30:27 +0900

    delete test code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import express from "express";
-import test from "./Test/index";
 import user from "./User/index";
 
 export default (app: express.Application) => {
-  app.use("/api/test", test);
   app.use("/api/user", user);
 };
--- src/routes/test/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getTest } from "./Test";
-
-const router: Router = Router();
-
-router.get("/", getTest);
-
-export default router;
