de222b95641a8c2a4cfa02c4ca488b3dcad6eec6
chaeyoo <chaeh0124@gmail.com>
2023-12-26 20:52:26 +0900

    [FEAT] 테이블 추가

---
+++ src/entities/file.entity.ts
@@ -0,0 +1,68 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Timestamp,
+} from "typeorm";
+
+@Entity({ synchronize: true, schema: "play" })
+export class File {
+  @PrimaryGeneratedColumn({
+    type: "bigint",
+    name: "id",
+    comment: "id",
+  })
+  id!: number;
+
+  @Column("varchar", {
+    name: "name",
+    comment: "파일명",
+    length: 200,
+  })
+  name!: string;
+
+  @Column("varchar", {
+    name: "extension",
+    comment: "확장자",
+    length: 45,
+  })
+  extension!: string;
+
+  @Column("varchar", {
+    name: "path",
+    comment: "경로",
+    length: 500,
+  })
+  path!: string;
+
+  @Column({
+    type: "bigint",
+    name: "created_by",
+    comment: "등록사용자 id",
+  })
+  createdBy!: number;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    name: "created_dt",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    comment: "생성일시",
+  })
+  createdDt!: Date;
+
+  @Column({
+    type: "bigint",
+    name: "updated_by",
+    comment: "수정사용자 id",
+  })
+  updatedBy!: number;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    name: "updated_dt",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    comment: "수정일시",
+  })
+  updatedDt!: Date;
+}
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -17,6 +17,29 @@ export class User {
 
   @Column({
     type: "varchar",
+    name: "email",
+    comment: "이메일",
+    length: 45,
+  })
+  email!: string;
+
+  @Column({
+    type: "varchar",
+    name: "password",
+    comment: "비밀번호",
+    length: 45,
+  })
+  password!: string;
+
+  @Column("varchar", {
+    name: "name",
+    comment: "이름",
+    length: 45,
+  })
+  name!: string;
+
+  @Column({
+    type: "varchar",
     name: "nick_name",
     comment: "닉네임",
     length: 45,
@@ -24,57 +47,100 @@ export class User {
   nickName!: string;
 
   @Column({
-    type: "varchar",
-    name: "email",
-    comment: "이메일",
-    length: 45,
+    type: "bigint",
+    name: "team_id",
+    comment: "팀 id",
   })
-  email!: string;
+  groupId!: number;
+
+  @Column({
+    type: "bigint",
+    name: "profile_img_file_id",
+    comment: "프로필 이미지 파일 id",
+  })
+  profileImgFileId!: number;
 
   @Column("varchar", {
-    name: "name",
-    comment: "",
+    name: "user_stts_cd",
+    comment: "사용자 상태",
     length: 45,
   })
-  name!: string;
+  userSttsCd!: string;
+
+  @Column("varchar", {
+    name: "stts_message",
+    comment: "상태 메시지",
+    length: 500,
+  })
+  sttsMessage!: string;
+
+  @Column({
+    type: "bigint",
+    name: "dept_id",
+    comment: "부서 id",
+  })
+  deptId!: number;
+
 
   @Column({
     type: "varchar",
-    name: "password",
-    comment: "",
-    length: 45,
+    name: "position",
+    comment: "직책",
+    length: 50,
   })
-  password!: string;
+  position!: string;
 
   @Column({
     type: "varchar",
     name: "birth",
-    comment: "",
+    comment: "생년월일",
     length: 45,
   })
   birth!: string;
 
   @Column({
     type: "varchar",
+    name: "phone",
+    comment: "휴대폰번호",
+    length: 45,
+  })
+  phone!: string;
+
+  @Column({
+    type: "varchar",
     name: "gender",
-    comment: "",
+    comment: "성별",
     length: 45,
   })
   gender!: string;
+  
+  @Column({
+    type: "bigint",
+    name: "created_by",
+    comment: "등록사용자 id",
+  })
+  createdBy!: number;
 
   @CreateDateColumn({
     type: "timestamp",
-    name: "regDt",
+    name: "created_dt",
     default: () => "CURRENT_TIMESTAMP(6)",
     comment: "생성일시",
   })
-  regDt!: Date;
+  createdDt!: Date;
+
+  @Column({
+    type: "bigint",
+    name: "updated_by",
+    comment: "수정사용자 id",
+  })
+  updatedBy!: number;
 
   @CreateDateColumn({
     type: "timestamp",
-    name: "modDt",
+    name: "updated_dt",
     default: () => "CURRENT_TIMESTAMP(6)",
-    comment: "생성일시",
+    comment: "수정일시",
   })
-  modDt!: Date;
+  updatedDt!: Date;
 }
